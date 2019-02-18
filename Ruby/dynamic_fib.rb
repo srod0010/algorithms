06a618cd8df27251d227b4f13eb58932f5e06902
Savio Rodrigues <saviofrod@gmail.com>
2019-02-18 14:07:08 -0800

    Implement dynamic fibonnaci using hash

---
+++ Ruby/dynamic_fib.rb
@@ -0,0 +1,19 @@
+class Fibonacci
+  def initialize
+    @cache = { 1 => 1, 2 => 1 }
+  end
+
+  def fibonacci(n)
+    # return 1 if n == 1 || n == 2
+    # Check our cache instead of the original base case
+    return @cache[n] unless @cache[n].nil?
+
+    # Record our answer in our cache before returning it
+    ans = fibonacci(n - 1) + fibonacci(n - 2)
+    @cache[n] = ans
+    return ans
+  end
+end
+
+test = Fibonacci.new
+puts test.fibonacci(100);
