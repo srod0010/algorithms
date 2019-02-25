38ef104414b7a95d1d4008d07129406240eff01b
Savio Rodrigues <saviofrod@gmail.com>
2019-02-25 11:28:51 -0800

    curry avg

--- JavaScript/prac.js
+++ JavaScript/prac.js
@@ -1,8 +1,29 @@
-var add = function (a) {
-    return function(b) {
-        return a + b;
+// var add = function (a) {
+//     return function(b) {
+//         return a + b;
+//     }
+// }
+
+// var addToFive = add(5);
+// console.log(addToFive(7));
+
+const ave = function(...n) {
+    let tot = 0;
+    for (let i = 0; i < n.length; i++) {
+        tot += n[i];
+    }
+
+    return tot/n.length
+}
+
+// console.log(ave(1,2,3,4))
+
+const spiceUp = function(fn, ...n) {
+    let that = this;
+    return function(...m) {
+        return fn.call(that, ...n.concat(m))
     }
 }
 
-var addToFive = add(5);
-console.log(addToFive(7));
+const doAvg = spiceUp(ave, 1,2,3);
+// console.log(doAvg(2,3,4,5,6))
