94154422b4679149d777eb64528a4a8748c7a87d
Savio Rodrigues <saviofrod@gmail.com>
2019-03-01 15:28:58 -0800

    in order, post order, pre order

---
+++ JavaScript/bst_traversal.js
@@ -0,0 +1,35 @@
+function inOrderTraverse(tree, array) {
+    if (tree.left) {
+        inOrderTraverse(tree.left, array)
+    }
+    array.push(tree.value)
+    if (tree.right) {
+        inOrderTraverse(tree.right, array)
+    }
+
+    return array
+}
+
+function preOrderTraverse(tree, array) {
+    if (tree !== null) {
+        array.push(tree.value)
+        preOrderTraverse(tree.left, array)
+        preOrderTraverse(tree.right, array)
+    }
+
+
+
+    return array;
+
+}
+
+function postOrderTraverse(tree, array) {
+    if (tree.left) {
+        postOrderTraverse(tree.left, array)
+    }
+    if (tree.right) {
+        postOrderTraverse(tree.right, array)
+    }
+    array.push(tree.value)
+    return array
+}
