a888abae2051b87f6da32906554197db0c26d76d
Savio Rodrigues <saviofrod@gmail.com>
2019-03-12 16:52:52 -0700

    update solution for negative num edge case

--- JavaScript/isEven.js
+++ JavaScript/isEven.js
@@ -1,5 +1,5 @@
 function isEven(num) {
-   if (num < 0) return false; 
+   if (num < 0) return isEven(-num); 
    if (num == 0) return true;
    if (num == 1) return false;
    return isEven(num-2);
