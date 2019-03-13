1852aeb0bd8a73a88d0f68205af0f34acf6c933e
Savio Rodrigues <saviofrod@gmail.com>
2019-03-13 10:48:09 -0700

    added full recursive solution

--- JavaScript/flatten.js
+++ JavaScript/flatten.js
@@ -5,9 +5,10 @@ function flatten(data) {
     for (let i = 0; i < data.length; i++) {
         if (Array.isArray(data[i])) {
             let level = flatten(data[i]);
-            for (num of level) {
-                flat.push(num);
-            }
+            flat.push(...level);
+            // for (num of level) {
+            //     flat.push(num);
+            // }
         } else {
             flat.push(data[i]);
         }
@@ -15,11 +16,28 @@ function flatten(data) {
     return flat;
 }
 
+// function flatten1(data) {
+//         //alternate solution
+//         //if data is not an array return [ data ]
+//     if (!Array.isArray(data)) return [data];
+//     let allEl = [];
+        
+//         //otherwise we have an array
+//     data.forEach(el => {
+//         //we are not sure if data is el or nested array, but either way we want to return flattened version
+//         let flattened = flatten1(el);
+//             //push with spread operator to ensure it is flat
+//         allEl.push(...flattened);
+//     });
+
+//     return allEl;
+// }
+
 array_1 = [1, 2, [[3, 4], [5, [6]]], [7, 8]];
-console.log(flatten(array_1)) // => [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+console.log(flatten1(array_1)) // => [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 
 array_2 = ['this', ['problem', 'is'], [['pretty', 'tough'], [[':)']]]];
-console.log(flatten(array_2)); // => [ 'this', 'problem', 'is', 'pretty', 'tough', ':)' ]
+console.log(flatten1(array_2)); // => [ 'this', 'problem', 'is', 'pretty', 'tough', ':)' ]
 
 
-console.log(flatten('base case')) // => [ 'base case' ]
+console.log(flatten1('base case')) // => [ 'base case' ]
