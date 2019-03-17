d388f4f48a1179ca3ff24ebd01e9a8ebc64abfea
Savio Rodrigues <saviofrod@gmail.com>
2019-03-17 12:07:53 -0700

    memoized solution

--- JavaScript/factorial.js
+++ JavaScript/factorial.js
@@ -5,3 +5,19 @@ function factorial(n) {
 }
 
 console.log(factorial(4))
+
+
+function factorial1(n, cache = {}) {
+    if(cache[n]) return cache[n];
+    if (n === 0 || n === 1) {
+        cache[n] = 1;
+        return cache[n];
+    }
+   
+
+    let ans = n * factorial1(n-1, cache);
+    cache[n] = ans;
+    return cache[n];
+}
+
+console.log(factorial1(5))
