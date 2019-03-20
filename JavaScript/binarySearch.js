ff847fce9831fc1a50e687c289ee0e3efac4990d
Savio Rodrigues <saviofrod@gmail.com>
2019-03-20 11:31:47 -0700

    add boolean version

--- JavaScript/binarySearch.js
+++ JavaScript/binarySearch.js
@@ -14,6 +14,23 @@ function binarySearch(arr, target) {
     }
 }
 
+// function binarySearch1(arr,target) {
+//     if (arr.length === 0) return false;
+//     let midIndex = Math.floor(arr.length / 2);
+//     let el = arr[midIndex];
+//     let leftHalf = arr.slice(0,midIndex);
+//     let rightHalf = arr.slice(midIndex+1);
+
+//     if (el > target) {
+//         return binarySearch1(leftHalf,target);
+//     } else if (el < target) {
+//         return binarySearch1(rightHalf,target);
+
+//     } else {
+//         return true;
+//     }
+// }
+
 console.log(binarySearch([1,2,3,4,5],4));
 console.log(binarySearch([1,2,3,4,5],2));
-console.log(binarySearch([1,2,3,4,5],3));
+console.log(binarySearch([1,2,3,4,5],7));
