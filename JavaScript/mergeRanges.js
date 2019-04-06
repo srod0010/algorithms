7149c39b2c470b287ecc68400447ab6db2357e1f
Savio Rodrigues <saviofrod@gmail.com>
2019-04-06 11:19:29 -0700

    complete with nlogn time

---
+++ JavaScript/mergeRanges.js
@@ -0,0 +1,21 @@
+function mergeRanges(meetings) {
+    const sortedMeetings = meetings.sort((a, b) => {
+        return a.startTime - b.startTime;
+    });
+
+    const mergedMeetings = [sortedMeetings[0]];
+
+    for (let i = 1; i < sortedMeetings.length; i++) {
+        const currentMeeting = sortedMeetings[i];
+        const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+
+        if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+            lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime)
+        } else {
+            mergedMeetings.push(currentMeeting);
+        }
+    }
+
+    return mergedMeetings;
+}
